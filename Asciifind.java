4c8abbfc6db9917b744fde749cb3a5a524907392
Aryanrajj01 <117764308+Aryanrajj01@users.noreply.github.com>
2023-01-11 18:57:21 +0530

    Create Asciifind.java

---
+++ Asciifind.java
@@ -0,0 +1,11 @@
+public class ascii {
+    public static void main(String[] args) {
+        for(char i = 65;i<91;i++){
+            System.out.println(i);
+        }
+        for(char i = 97;i<=122;i++){
+            System.out.println(i);
+        }
+    }
+    
+}
