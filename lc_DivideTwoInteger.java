e9003e186c20c53eeb52809ca43c65dabc782625
Aryanrajj01 <117764308+Aryanrajj01@users.noreply.github.com>
2023-01-17 23:49:15 +0530

    Create lc_DivideTwoInteger.java

---
+++ lc_DivideTwoInteger.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int divide(int dividend, int divisor) {
+        int d;
+        
+        d = dividend/divisor;
+        //System.out.println(d);
+        return d;
+    }
+}
