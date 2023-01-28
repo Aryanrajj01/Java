a44cfd2228ba9dce45ef6ea3ed0a02343801034a
Aryanrajj01 <117764308+Aryanrajj01@users.noreply.github.com>
2023-01-28 23:56:24 +0530

    Create arrayInput.java

---
+++ arrayInput.java
@@ -0,0 +1,17 @@
+// user Input array in Java!!!!!!
+
+import java.util.*;
+public class array1{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        for(int i = 0;i<size;i++){
+            arr[i] = sc.nextInt();
+        }
+        for(int i = 0;i<size;i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
