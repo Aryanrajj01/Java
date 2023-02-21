d7bd95ae226f06ddc4170bf25c577af938fc3ea8
Aryanrajj01 <117764308+Aryanrajj01@users.noreply.github.com>
2023-02-21 21:08:15 +0530

    Create lc_NoOfSubarray.java

---
+++ lc_NoOfSubarray.java
@@ -0,0 +1,8 @@
+class Solution {
+    public double[] convertTemperature(double c) {
+        double a = c + 273.15;
+        double b = c *(1.80) + 32.00;
+        return new double[]{a,b};
+
+    }
+}
