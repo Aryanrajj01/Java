c14f43493523832e2f2284dbbfbb8a953c1dc3f5
Aryan Rajput <117764308+Aryanrajj01@users.noreply.github.com>
2023-03-20 19:38:34 +0530

    Create lc_CanPlaceFlower.java

---
+++ lc_CanPlaceFlower.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean canPlaceFlowers(int[] f, int n) {
+    int c = 0;
+    for(int i = 0 ;i<f.length;i++){
+        if(f[i] == 0 && (i == 0 || f[i-1] == 0) && (i == f.length-1 || f[i+1] == 0)){
+            c++;
+            f[i] = 1;
+            if(c>=n){
+                return true;
+            }
+        }  
+    }
+    
+    return c>=n;
+    }
+}
