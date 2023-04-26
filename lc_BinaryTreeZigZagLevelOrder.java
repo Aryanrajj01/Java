fda2b9039863034929e4bcd7c65eda43d8304562
Aryan Rajput <117764308+Aryanrajj01@users.noreply.github.com>
2023-04-27 02:43:15 +0530

    Create lc_BinaryTreeZigZagLevelOrder.java

---
+++ lc_BinaryTreeZigZagLevelOrder.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        List<List<Integer>> ans = new LinkedList<List<Integer>>();
+        if(root==null)return ans;
+        q.offer(root);
+        while(!q.isEmpty()){
+            int a = q.size();
+            List<Integer> t = new LinkedList<Integer>();
+            for(int i =0;i<a;i++){
+                    if(q.peek().left!=null){
+                    q.offer(q.peek().left);
+                    }
+                    if(q.peek().right!=null){
+                        q.offer(q.peek().right);
+                    }
+                    t.add(q.poll().val);
+            }
+            ans.add(t);
+        }
+        for(int i = 0;i<ans.size();i++){
+            if(i%2!=0){
+                 Collections.reverse(ans.get(i));
+            }
+        }
+        return ans;
+    }
+}
