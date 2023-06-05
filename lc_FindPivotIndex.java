d9fd4ee8f98567e953b51887466aa12be39c8b78
Aryan Rajput <117764308+Aryanrajj01@users.noreply.github.com>
2023-06-05 23:22:52 +0530

    Create lc_FindPivotIndex.java

---
+++ lc_FindPivotIndex.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int sum = 0, ls = 0;
+        for (int x: nums){
+             sum += x;
+        }
+        for (int i = 0; i < nums.length; ++i) {
+            if (ls == sum - ls - nums[i]){
+            return i;
+            }
+            ls += nums[i];
+        }
+        return -1;
+    }
+}
