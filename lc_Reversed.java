13353efbb5ad5579ab4cd9e29d954c8b13e56285
Aryan Rajput <117764308+Aryanrajj01@users.noreply.github.com>
2023-07-12 23:11:30 +0530

    Create lc_Reversed.java

---
+++ lc_Reversed.java
@@ -0,0 +1,9 @@
+public class Main{
+public static void main(String[] args){
+  String s = "Hello";
+  String a = " ";
+  for(int i = 0i<s.length();i++){
+a = s.charAt(i) + a;}
+  Sytem.out.println(a);
+}
+}
