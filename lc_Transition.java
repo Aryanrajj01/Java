d51c09f4c2897f10c03686f8f2bd80a4a4ffbeef
Aryan Rajput <117764308+Aryanrajj01@users.noreply.github.com>
2023-07-13 22:59:04 +0530

    Create lc_Transition.java

---
+++ lc_Transition.java
@@ -0,0 +1,10 @@
+public class Transition{
+  public static void main(String[] args){
+    int TransitionPoint(int arr[],int n){
+      for(int i=0;i<n;i++){
+if(arr[i]==1){
+  return i;
+}
+      }
+      return -1;}
+  }
