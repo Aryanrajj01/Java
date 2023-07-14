abc88cb1af716a42c8ead56edaafddacb7946218
Aryan Rajput <117764308+Aryanrajj01@users.noreply.github.com>
2023-07-14 23:24:12 +0530

    Create FinallyExecute.java

---
+++ FinallyExecute.java
@@ -0,0 +1,12 @@
+public class Main{
+  public static void main(String[] args){
+int n1 = 10,n2 = 0,n3 = 0;
+    try{
+      n3 = n1/n2;
+    }
+    finally{
+  System.out.println("Finally Executed");
+      System.exie(0);
+    }
+  }
+}
