be9189f712bde67798587f4cc8f1627bad6559c5
Aryan Rajput <117764308+Aryanrajj01@users.noreply.github.com>
2023-08-05 23:33:07 +0530

    Create array.java

---
+++ array.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;  
+public class array 
+{  
+public static void main(String[] args)   
+{  
+int[] a1 = {23,45,12,78,4,90,1};        
+int[] a2 = {77,11,45,88,32,56,3};  
+for(int i= 0;i<a1.length;i++){
+    a2[i] = a1[i];
+    System.out.println(a1);
