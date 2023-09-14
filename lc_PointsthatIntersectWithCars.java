275b737bf98934b7f70948f6e0442cf1a16eaf20
Aryan Rajput <117764308+Aryanrajj01@users.noreply.github.com>
2023-09-14 23:16:26 +0530

    Create lc_PointsthatIntersectWithCars.java

---
+++ lc_PointsthatIntersectWithCars.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numberOfPoints(List<List<Integer>> nums) {
+        /*int[] a=new int[101];
+        for(int i = 0;i<nums.size();i++){
+            for(int j = nums.get(i).get(0);j<=nums.get(i).get(1);j++){
+                a[j]++;
+            }
+        }
+        int c=0;
+        for(int i:a){
+…            }
+        }
+        return l.size();
+    }
+}
