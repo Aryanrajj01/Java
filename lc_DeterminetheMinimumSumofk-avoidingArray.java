d10439fdaf102d843687362462c9d061ca75b770
Aryan Rajput <117764308+Aryanrajj01@users.noreply.github.com>
2023-09-26 20:11:52 +0530

    Create lc_DeterminetheMinimumSumofk-avoidingArray.java

---
+++ lc_DeterminetheMinimumSumofk-avoidingArray.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minimumSum(int n, int k) {
+        /*int ans = 0;
+        Set<Integer> se = new HashSet<>();
+        int idx = 1;
+        while(n-->0){
+        if(se.contains(k-idx)){
+        n++;
+        }else{
+           se.add(idx);
+…    }
+}
