4875d29ae931b26165c63fd0e07b9a8e1c714e2b
Aryan Rajput <117764308+Aryanrajj01@users.noreply.github.com>
2023-10-04 23:21:46 +0530

    Create lc_Valid Perfect Square.java

---
+++ lc_Valid Perfect Square.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        if(num == 1){
+            return true;
+        }
+        int start = 1;
+        int end = num;
+                                                       
+        while(start<=end){
+            long   mid = start + (end-start)/2;
+…        
+    }
+}
