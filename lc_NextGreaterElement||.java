1478bdde9a0cd40d0efc26d065d85d55f8806d4a
Aryan Rajput <117764308+Aryanrajj01@users.noreply.github.com>
2023-10-26 23:00:40 +0530

    Create lc_NextGreaterElement||.java

---
+++ lc_NextGreaterElement||.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        /*int m = nums[0];
+        int[] a = new int[nums.length];
+       for(int i = 0;i<nums.length;i++){
+           if(nums[i]>m){
+               m = nums[i];
+           }
+       } 
+       m = -1;
+…             }
+         }
+     } 
+     return ans;
+    }
+}
